4914f95a502a41cd04322616c091719674829b4b
Szymon Muszyński <s.muszynski2003@gmail.com>
2024-04-13 15:47:05 +0200

    2. Poprawienie bledow zwiazanych z formatowaniem kodu w klasie HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,18 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+	private int totalRejected = 0;
 
     public int reportRejected() {
-        return totalRejected;
+    	return totalRejected;
     }
 
+    @Override
     public void countIn(int in) {
-    if (!callCheck() && in > peekaboo())
-            totalRejected++;
-            else
-                super.countIn(in);
+    	if (!callCheck() && in > peekaboo()) {
+    		totalRejected++;
+	    } else {
+	    	super.countIn(in);
+	    }
     }
 }
